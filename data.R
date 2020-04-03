84107b18af113882f52671a4636e85dd7d753ad6
Gordon Shotwell <gordon@shotwell.ca>
2020-04-03 09:30:24 -0300

    Added drake

--- data.R
+++ data.R
@@ -1,7 +1,8 @@
-covid <- readr::read_csv("https://novascotia.ca/coronavirus/data/COVID-19-data.csv",
-                     skip = 1)
-covid$Hospitalized <- NULL
-covid <- covid[, 1:6]
-names(covid) <- c("date", "new_cases", "negatives", "recovered", "in_hospital", "deaths")
-covid$positives <- cumsum(covid$new_cases)
-write.csv(covid, "ns_daily_covid.csv")
+process_covid <- function(covid) {
+  covid$Hospitalized <- NULL
+  covid <- covid[, 1:6]
+  names(covid) <- c("date", "new_cases", "negatives", "recovered", "in_hospital", "deaths")
+  covid$positives <- cumsum(covid$new_cases)
+  write.csv(covid, "ns_daily_covid.csv")
+  saveRDS(covid, "covid.Rds")
+}
