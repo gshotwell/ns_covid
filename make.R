0db171a593ab7632e2fab2e2937f3bf33b341045
Gordon Shotwell <gordon@shotwell.ca>
2020-04-03 14:21:43 -0300

    data table

--- make.R
+++ make.R
@@ -1,7 +1,8 @@
 library(drake)
 source("data.R")
+download.file("https://novascotia.ca/coronavirus/data/COVID-19-data.csv", "input.csv")
 plan <- drake_plan(
-  raw_data = readr::read_csv("https://novascotia.ca/coronavirus/data/COVID-19-data.csv",
+  raw_data = readr::read_csv("input.csv",
                                skip = 1),
   processed = process_covid(raw_data),
   report = rmarkdown::render(
