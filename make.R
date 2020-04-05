f664142e5c34296020b6ca722766b392f2e3ab12
gordon <gordon@socure.com>
2020-04-05 14:11:38 -0300

    auto-update

--- make.R
+++ make.R
@@ -1,15 +1,26 @@
 library(drake)
 library(git2r)
+library(RCurl)
 source("data.R")
-download.file("https://novascotia.ca/coronavirus/data/COVID-19-data.csv", "input.csv")
+url = "https://novascotia.ca/coronavirus/data/COVID-19-data.csv"
+xx = getURL(url, nobody=1L, header=1L)
+a <- strsplit(xx, "\r\n")
+writeLines(a[[1]][5], "last-modified")
+
+download_data <- function(last_mod) {
+  download.file("https://novascotia.ca/coronavirus/data/COVID-19-data.csv", "input.csv")
+}
+
+
 update_git <- function(output) {
   repo <- repository()
   git2r::commit(repo, all = TRUE, message = "auto-update")
 }
 
 plan <- drake_plan(
-  raw_data = readr::read_csv("input.csv",
-                               skip = 1),
+  last_mod = readLines("last-modified"),
+  download = download_data(last_mod),
+  raw_data = read_csv("input.csv",  skip = 1),
   processed = process_covid(raw_data),
   report = rmarkdown::render(
     params = list(data = processed),
