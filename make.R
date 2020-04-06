3c40092f91fe21eaf37b7e2cd3b4acd0e779060b
Gordon Shotwell <gordon@shotwell.ca>
2020-04-06 10:04:59 -0300

    plotly

--- make.R
+++ make.R
@@ -2,14 +2,9 @@ library(drake)
 library(git2r)
 library(RCurl)
 source("data.R")
-url = "https://novascotia.ca/coronavirus/data/COVID-19-data.csv"
-xx = getURL(url, nobody=1L, header=1L)
-a <- strsplit(xx, "\r\n")
-writeLines(a[[1]][5], "last-modified")
 
-download_data <- function(last_mod) {
-  download.file("https://novascotia.ca/coronavirus/data/COVID-19-data.csv", "input.csv")
-}
+download.file("https://novascotia.ca/coronavirus/data/COVID-19-data.csv", "input.csv")
+
 
 
 update_git <- function(output) {
@@ -18,8 +13,6 @@ update_git <- function(output) {
 }
 
 plan <- drake_plan(
-  last_mod = readLines("last-modified"),
-  download = download_data(last_mod),
   raw_data = read_csv("input.csv",  skip = 1),
   processed = process_covid(raw_data),
   report = rmarkdown::render(
