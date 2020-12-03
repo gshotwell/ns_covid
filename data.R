f5e4e56157d1e8c7964a26d31546015883d4c72b
Gordon Shotwell <gordon@shotwell.ca>
2020-12-03 13:05:25 -0400

    dec 3

--- data.R
+++
@@ -1,8 +0,0 @@
-process_covid <- function(covid) {
-  covid$Hospitalized <- NULL
-  covid <- covid[, 1:6]
-  names(covid) <- c("date", "new_cases", "negatives", "recovered", "in_hospital", "deaths")
-  covid$positives <- cumsum(covid$new_cases)
-  write.csv(covid, "ns_daily_covid.csv")
-  saveRDS(covid, "covid.Rds")
-}
